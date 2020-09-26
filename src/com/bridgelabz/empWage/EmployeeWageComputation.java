fd6a3be2154343658ec91e8907b6bdf6f942c561
Kauk777 <curokky777@gmail.com>
2020-09-26 20:16:43 +0530

    [KAUSHAL]Added: UC14 Total Employee Wage by Company

--- src/com/bridgelabz/empWage/EmployeeWageComputation.java
+++ src/com/bridgelabz/empWage/EmployeeWageComputation.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.empWage;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class EmployeeWageComputation implements IempWage {
 
@@ -9,21 +11,29 @@ public class EmployeeWageComputation implements IempWage {
 
 	  private int noOfCompany=0;
 	  private LinkedList<EmpWage> companyEmpWageList;
+	  private Map<String,EmpWage> companyToEmpWageMap;
 	 
 	  public EmployeeWageComputation() {
 	    companyEmpWageList=new LinkedList<EmpWage>();
+	    companyToEmpWageMap=new HashMap<>();
 	 }
 	  
 	  public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 	    EmpWage companyEmpWage=new EmpWage(company,empRatePerHour,numOfWorkingDays,maxHoursPerMonth);
 	    companyEmpWageList.add(companyEmpWage);
+	    companyToEmpWageMap.put(company, companyEmpWage);
 	 }
 	  public void computeEmpWage() {
 	    for(int i=0;i<companyEmpWageList.size();i++) {
 	    	EmpWage companyEmpWage=companyEmpWageList.get(i);
 	        companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
 	        System.out.println(companyEmpWage);
-	  }
+	    }
+	 }
+	  
+	 public int getTotalWage(String company) {
+		 return companyToEmpWageMap.get(company).totalEmpWage;
+		 
 	 }
 	 public int computeEmpWage(EmpWage companyEmpWage) {
 	     int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
@@ -46,11 +56,11 @@ public class EmployeeWageComputation implements IempWage {
 	   return totalEmpHrs*companyEmpWage.empRatePerHour;
 	 }
 	 public static void main(String[] args) {
-		EmployeeWageComputation empWageMul=new EmployeeWageComputation();
+		IempWage empWageMul=new EmployeeWageComputation();
 	    empWageMul.addCompanyEmpWage("DMart", 20, 2, 10);
 	    empWageMul.addCompanyEmpWage("Reliance", 10, 4, 20);
-	    empWageMul.addCompanyEmpWage("BigBasket", 30, 6, 25);
 	    empWageMul.computeEmpWage();
+	    System.out.println("total Wage for DMart comapny is "+empWageMul.getTotalWage("DMart"));
 	}
 
 }
--- src/com/bridgelabz/empWage/IempWage.java
+++ src/com/bridgelabz/empWage/IempWage.java
@@ -3,5 +3,6 @@ package com.bridgelabz.empWage;
 public interface IempWage {
 	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
 	public void computeEmpWage();
+	public int getTotalWage(String company);
 
 }
