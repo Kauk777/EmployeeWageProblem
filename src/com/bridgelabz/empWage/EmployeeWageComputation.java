6b86bdc16af61e2b8febdbca67f42ee54dbc40af
Kauk777 <curokky777@gmail.com>
2020-09-26 16:38:01 +0530

    [KAUSHAL]Added: Welcome message

---
+++ src/com/bridgelabz/empWage/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.empWage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Problem");
+
+	}
+
+}
